2c3fe3bfe2629e395d0da04a606e429a82bf7045
Ulhve <ulhve@mail.ru>
2023-05-05 17:33:31 +0300

    Update WebServiceConfiguration.java

--- server/src/main/java/com/example/shop/configuration/WebServiceConfiguration.java
+++ server/src/main/java/com/example/shop/configuration/WebServiceConfiguration.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class WebServiceConfig {
+public class WebServiceConfiguration {
     @Autowired
     private Bus bus;
 
